994ed28b50798445b841e4c2b519835f06bc1989
oloruntobi <awoderuoloruntobi@gmail.com>
2019-07-17 16:08:34 +0100

    Completed one of the stretch

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ function getCarById(id){
 function createNewCar({ vin, make, model, mileage, transmission_type, transmission_style }){
     return db('cars').insert({ vin, make, model, mileage, transmission_type, transmission_style })
 }
+function deleteCar(id){
+    return db('cars').where({ id }).del();
+}
+
+function updateCar(id, { vin, make, model, mileage, transmission_type, transmission_style }){
+    return db('cars').where({ id }).update({ vin, make, model, mileage, transmission_type, transmission_style });
+}
 
 server.get('/cars', async (req, res) => {
     try {
@@ -53,22 +60,25 @@ server.post('/cars', async (req, res) => {
 })
 
 
+server.delete('/cars/:id', async (req, res) => {
+    try {
+        const deletedCar = await deleteCar(req.params.id);
+        res.status(200).json({message: `${deletedCar} car has been deleted`})
+    } catch (error) {
+        res.status(500).json({ message: 'could not delete this car' })
+    }
+})
 
+server.put('/cars/:id', async (req, res) => {
+    try {
+        const { vin, make, model, mileage, transmission_type, transmission_style } = req.body;
+        const result = await updateCar(req.params.id, { vin, make, model, mileage, transmission_type, transmission_style });
+        const carUpdated = await getCarById(req.params.id);
+        res.status(200).json(carUpdated)
+    } catch (error) {
+        res.status(500).json({ message: 'could not update car'})
+    }
+})
 
-server.get('/', async (req, res) => {
-  try {
-    res.json('success');
-  } catch (error) {
-    next(new Error('argh!'));
-  }
-});
-
-server.use((err, req, res) => { 
-  console.error('ERROR:', err);
-  res.status(500).json({
-    message: err.message,
-    stack: err.stack,
-  });
-});
 
 module.exports = server;
